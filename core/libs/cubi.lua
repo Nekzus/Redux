532d59959d31a4c786561c5da872f4f09cceca0d
Nekzt#7357 <willianrazera@outlook.com>
2019-10-30 14:55:43 -0300

    ++

---
+++ core/libs/cubi.lua
@@ -0,0 +1,47 @@
+local effects = {
+	["self"] = {"eu", "sou", "estou", "quero"},
+	["want"] = {"quero", "meu", "minha"},
+	["funny"] = {"kk", "hah", "aha", "auh", "auah", "hua", "hau", "rs", "shu", "sah"},
+}
+
+local positive = {
+	"entendo",
+	"concordo",
+	"apoio",
+
+}
+
+function cubi(text, force)
+
+end
+
+-- https://www.normaculta.com.br/classes-gramaticais/
+
+-- Essenciais
+--[[
+[] = {
+	"Eu"
+}
+[] = {
+	"não sei",
+	"não ligo",
+	"não me importo"
+}
+[] = {
+	"entendo",
+	"concordo",
+	"apoio"
+}
+
+-- Complementares
+[] = {
+	"e não quero saber",
+	"e não ligo",
+	"e caguei pra você"
+}
+[] = {
+	"e espero que dê tudo certo",
+	"e espero que funcione",
+	"e gostaria de entender melhor em algum momento",
+}
+]]
