8c5da48d65f8bb9a551b26a66f1288ecbada2159
Trico <uzumakenutro@hotmail.com>
2019-11-15 22:51:40 -0300

    +

--- addons/base/translate.lua
+++ addons/base/translate.lua
@@ -60,8 +60,9 @@ local _function = function(data)
 				pages = max(1, tonumber(tostring(pages):match("%d+") + 1))
 			end
 
-			embed:field({name = localize("${translationCodes} (%s/%s) [${page} %s/%s]", guildLang, inPage, listTotal, page, pages), value = (result ~= "" and result or localize("${noResults}", guildLang))})
-
+			embed:title(localize("${translationCodes} (%s/%s) [${page} %s/%s]", guildLang, inPage, listTotal, page, pages))
+			embed:description(result ~= "" and result or localize("${noResults}", guildLang))
+			
 			embed:color(config.colors.blue)
 			embed:footerIcon(config.images.info)
 			signFooter(embed, data.author, guildLang)
