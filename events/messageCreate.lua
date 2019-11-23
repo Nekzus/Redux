d822f374dd496935a0a07a254a686b9eb907254f
Trico <uzumakenutro@hotmail.com>
2019-11-23 17:26:59 -0300

    ata

--- events/messageCreate.lua
+++ events/messageCreate.lua
@@ -235,14 +235,6 @@ client:on("messageCreate",
 					data.message.content -- Os argumentos utilizados
 				)
 			end
-
-			if (args[1]) then
-				if data.message.mentionedUsers.first = client.user then
-					local mensagem = data.channel:send(localize("${guildPrefix}", guildLang, guildData:raw().prefix))
-					wait(8)
-					data.message:delete(data.channel:getMessage(mensagem))
-				end
-			end
 		end
 	end
 )
