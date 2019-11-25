c594a121c3868eafb04f40e3aa5762b37cd600ab
Nekzt#7357 <willianrazera@outlook.com>
2019-11-25 15:07:40 -0300

    ++

--- utils/getters/replyEmbed.lua
+++ utils/getters/replyEmbed.lua
@@ -3,15 +3,13 @@ function replyEmbed(text, message, method)
 	local private = isPrivateChannel(message.channel)
 	local guildData = not private and getGuildData(message.guild)
 	local guildLang = guildData and guildData:get("lang") or config.defaultGuild.lang
-	local color = {
-		method == "ok" and paint("green")
-		or method == "warn" and paint("yellow")
-		or method == "error" and paint("red")
-		or method == "no" and paint("red2")
-		or method == "info" and paint("blue")
-	}
 
-	embed:color(unpack(color))
+	embed:color(method == "ok" and paint("green")
+	or method == "warn" and paint("yellow")
+	or method == "error" and paint("red")
+	or method == "no" and paint("red2")
+	or method == "info" and paint("blue")
+	or paint())
 	embed:footerIcon(config.images[method] or message.author.avatarURL)
 	signFooter(embed, message.author, guildLang)
 
