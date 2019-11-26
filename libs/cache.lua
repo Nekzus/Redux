7eb742bacd1c491ac9f0666c511bbb7d78113628
Nekzt#7357 <willianrazera@outlook.com>
2019-11-26 17:06:06 -0300

    Um passo antes do merging do novo sistema da base de dados

--- libs/cache.lua
+++ libs/cache.lua
@@ -13,8 +13,8 @@
 ]]
 
 -- Cria um construtor para registrar os métodos e metamétodos
-local main = {}
-main.__index = main
+local _cache = {}
+_cache.__index = _cache
 
 -- Função para realizar o acesso e a criação dentro do vetor passado
 function access(list, paths, default, delete)
@@ -64,44 +64,38 @@ function access(list, paths, default, delete)
 end
 
 -- Função construtora que retorna ou cria os métodos dentro do vetor passado
-function main:__call(list)
-	if self.bin == nil then
+function _cache:__call(list)
+	if self.bin then
+		print("Thread is already created, use the metamethods")
+		return false
+	else
 		return setmetatable({
 			bin = type(list) == "table" and list or {}
-		}, main)
-	else
-		print("Thread is already created, use :get() or :set()")
-
-		return false
+		}, _cache)
 	end
 end
 
 -- Retorna o acesso dentro do vetor após a criação conforme passado via string
-function main:get(paths, default)
-	if self.bin == nil then
-		print("Must create a thread first, use cache(table)")
-
-		return false
-	else
+function _cache:get(paths, default)
+	if self.bin then
 		local result = access(self.bin, paths, default or {})
 
 		if result and type(result) == "table" then
 			return setmetatable({
 				bin = result
-			}, main)
+			}, _cache)
 		else
 			return result
 		end
+	else
+		print("Must create a thread first, use cache(table)")
+		return false
 	end
 end
 
 -- Define uma chave e um valor dentro do vetor atualmente acessado
-function main:set(key, value)
-	if self.bin == nil then
-		print("Must create a thread first, use cache(table)")
-
-		return false
-	else
+function _cache:set(key, value)
+	if self.bin then
 		if not key then
 			print("Key not found for table index in :set()")
 
@@ -109,22 +103,24 @@ function main:set(key, value)
 		end
 
 		self.bin[key] = value
-
 		return self.bin[key] or value
+	else
+		print("Must create a thread first, use cache(table)")
+		return false
 	end
 end
 
 -- Retorna o vetor principal
-function main:raw()
-	if self.bin == nil then
-		return false
-	else
+function _cache:raw()
+	if self.bin then
 		return self.bin
+	else
+		return false
 	end
 end
 
 -- Registra o processo
-cache = setmetatable({}, main)
+cache = setmetatable({}, _cache)
 
 -- Retorna o processo para confirmar que houve a execução sem erros
 return cache
--- libs/databaseNew.lua
+++ libs/databaseNew.lua
@@ -1,11 +1,29 @@
+--[[
+	Parte responsável por manusear o banco de dados local mantido no bot
+	Esse script abre novos canais para editar os dados, e após isso, fecha
+	quando for encerrada a utilização, dessa forma, mantendo o uso de memória
+	do bot o mais baixo possível
+
+	local info = db("./dir/file.ext")
+	local edit = info:open()
+	edit.var = 1
+	print(edit.var)
+	info:close()
+
+	edit = info:open()
+	print(edit.var)
+	info:close()
+]]
+
 -- Cria um construtor para registrar os métodos
-local main = {}
-main.__index = main
+local _db = {}
+_db.__index = _db
 
 -- Carrega os dados da modula serpent que converte um vetor em texto
-local mp = mp or require("./libs/msgPack.lua")
+local serpent = serpent or require("./libs/serpent.lua")
+local cache = cache or require("./libs/cache.lua")
 
--- Função local para checar se o caminho para um arquivo é válido
+-- Função para checar se o caminho para um arquivo é válido
 local function isFile(path)
 	local file = io.open(path, "rb")
 
@@ -17,73 +35,112 @@ local function isFile(path)
 	return false
 end
 
+-- Função para checar se um diretório é válido
+function isDir(path)
+   local success, err, code = os.rename(path, path)
+
+   if not success then
+      if code == 13 then
+         return true
+      end
+   end
+
+   return success, err
+end
+
+-- Função para serializar uma table
+local function serialize(data)
+	return serpent.dump(data)
+end
+
+-- Função para desserializar uma string
+local function deserialize(text)
+	return loadstring(text)()
+end
+
 -- Retorna o conteúdo de um arquivo
-local function loadFile(path)
+local function readFile(path)
 	assert(
 		path and isFile(path),
 		format("Path for file not found: %s", path)
 	)
 
 	local file = io.open(path, "rb")
-	local result
 
-	if file then
-		result = mp.unpack(file:read("*a"))
-		file:close()
+	if not file then
+		return nil
 	end
+
+	local result = file:read("*a")
+	file:close()
 
 	return result
 end
 
 -- Modifica o conteúdo de um arquivo
-local function saveFile(path, list)
-	assert(path and isFile(path), format("Path for file not found: %s", path))
-	assert(list and type(list), "List must be a table for saveFile")
+local function writeFile(path, text)
+	assert(
+		text and type(text) == "string",
+		format("Text must be a string in writeFile for path: %s", path)
+	)
 
-	local file = io.open(path, "wb")
+	local file = io.open(path, "w")
 
-	if file then
-		file:write(mp.pack(list))
-		file:close()
-		return true
+	if not file then
+		printf("Could not writeFile for path %s", path)
+		return false
 	end
 
-	return false
+	file:write(text)
+	file:close()
+
+	return true
 end
 
-function main:__call(path)
-	return setmetatable({
+function _db:__call(path, duration)
+	if not isFile(path) then
+		local fileName = path:split("/")
+		fileName = fileName[#fileName]
+		writeFile(path, serialize({}))
+	end
+
+	local mt = setmetatable({
 		path = path,
 		ticket = 0,
-	}, main)
+	}, _db)
+
+	if duration then
+		mt:open(type(duration) == "number" and duration or nil)
+	end
+
+	return mt
 end
 
-function main:open(duration)
-	print("OPENED EDIT")
+function _db:open(duration)
 	duration = duration or 30
-	self.data = self.data or loadFile(self.path)
+	self.data = self.data or cache(deserialize(readFile(self.path)))
 	self.ticket = self.ticket + 1
 	local ticket = self.ticket
 
 	if self.handler then
-		print("RENEWING TIMER")
 		self.handler:stop()
-		self.handler:close()
+		self.handler = nil
 	end
 
-	self.handler = timer.setTimeout(duration * 1000, function()
-		if self.ticket == ticket then
-			print("LIFETIME REACHED, AUTO-SAVING")
-			self:close()
-		end
-	end)
+	if duration <= 0 then
+		self.handler = timer.setTimeout(duration * 1000, function()
+			if self.ticket == ticket then
+				self:close()
+			end
+		end)
+	end
 
-	return self.data
+	return self
 end
 
-function main:close()
+function _db:close()
 	local success = assert(
-		saveFile(self.data),
+		writeFile(self.path, serialize(self.data:raw())),
 		"Could not close connection to database"
 	)
 
@@ -91,15 +148,48 @@ function main:close()
 	self.ticket = self.ticket + 1
 
 	if self.handler then
-		print("ENDING LIFETIME")
 		self.handler:stop()
-		self.handler:close()
+		self.handler = nil
 	end
-
-	self.handler = nil
-	print("EDIT CLOSED")
 
 	return true
 end
 
-ndb = main
+function _db:delete()
+	assert(self.path, "Must create thread first")
+
+	local success, err = os.remove(self.path)
+
+	if not success then
+		printf("Could not delete file at path %s because: %s", self.path, err)
+
+		return false
+	end
+
+	return true
+end
+
+function _db:set(key, value)
+	assert(self.path, "Must create thread first")
+	assert(self.data, "Must open channel first with method")
+
+	return self.data:set(key, value)
+end
+
+function _db:get(paths, default)
+	assert(self.path, "Must create thread first")
+	assert(self.data, "Must open channel first with method")
+
+	return self.data:get(paths, default)
+end
+
+function _db:raw()
+	assert(self.path, "Must create thread first")
+	assert(self.data, "Must open channel first with method")
+
+	return self.data:raw()
+end
+
+ndb = setmetatable({}, _db)
+
+return ndb
