9b1dd702c6461e51e82bf0cff53167c32481d270
Nekzt#7357 <willianrazera@outlook.com>
2019-12-24 11:37:47 -0300

    ++

--- utils/getters/alpha/getBytes.lua
+++ utils/getters/alpha/getBytes.lua
@@ -5,7 +5,7 @@ function getBytes(text)
 		local bytes = {string.byte(char, 1, -1)}
 
 		for _, charByte in next, bytes do
-			result = string.format("%s[\\%s]", result, charByte)
+			result = join(result, "\\", charByte)
 		end
 	end
 
