2550daa43e1ec5af91e57263fe6399c1a44be7d8
Nekzt#7357 <willianrazera@outlook.com>
2019-12-30 15:47:36 -0300

    Criada biblioteca de paginação para embeds, será implementada em breve

--- libs/book.lua
+++ libs/book.lua
@@ -1,12 +1,89 @@
---[[local methods = {}
+local methods = {}
 local metatable = {}
 
-function metatable:__call()
-
+function metatable:__index(...)
+	return rawget(methods, ...)
 end
 
-function metatable:__index(key)
+local function updatePages(list, perPage)
+	assert(type(list), "List must be a table")
+	assert(type(perPage), "PerPage must be a number")
 
+	local count = #list
+
+	return (count == 0 or perPage == 0 and 0)
+	or count / perPage
 end
 
-return book]]
+function metatable:__call(list)
+	return setmetatable({
+		list = list or {},
+		page = 1,
+		pages = list and updatePages(list) or 1,
+		perPage = 10,
+	}, metatable)
+end
+
+function methods:setPerPage(items)
+	assert(self.list, "Must create object first")
+	assert(type(items) == "number", "Items must be a number")
+
+	self.perPage = items
+end
+
+function methods:setList(list)
+	assert(self.list, "Must create object first")
+	assert(type(list) == "table", "List must be a table")
+
+	self.list = list
+	self.pages = updatePages(self.list, self.perPage)
+end
+
+function methods:insert(item)
+	assert(self.list, "Must create object first")
+	assert(item, "Must provide a valid item")
+
+	table.insert(self.list, item)
+	self.pages = updatePages(self.list, self.perPage)
+end
+
+function methods:remove(index)
+	assert(self.list, "Must create object first")
+	assert(type(index) == "number", "Index must be a number (table position)")
+
+	table.remove(self.list, index)
+	self.pages = updatePages(self.list, self.perPage)
+end
+
+function methods:setPage(page)
+	assert(self.list, "Must create object first")
+	assert(type(page) == "number", "Page must be a number")
+
+	self.page = page
+end
+
+function methods:next(increment)
+	assert(self.list, "Must create object first")
+
+	self:setPage(math.min(self.page + increment or 1, self.pages))
+end
+
+function methods:back(increment)
+	assert(self.list, "Must create object first")
+
+	self:setPage(math.max(self.page - increment or 1, self.pages))
+end
+
+function methods:sort(func)
+	assert(self.list, "Must create object first")
+
+	table.sort(self.list, func)
+end
+
+function methods:render()
+	return paginate(self.list, self.perPage, self.page)
+end
+
+book = setmetatable(methods, metatable)
+
+return book
--- utils/getters/paginate.lua
+++ utils/getters/paginate.lua
@@ -1,12 +1,10 @@
 function paginate(list, perPage, page)
-	perPage = perPage or 10
-	page = page or 1
-
+	local perPage = perPage or 10
+	local page = page or 1
 	local sliceTo = (page * perPage)
 	local sliceAt = (sliceTo - (perPage - 1))
-	local result = table.slice(list, sliceAt, sliceTo, 1)
 
-	return result
+	return table.slice(list, sliceAt, sliceTo, 1)
 end
 
 return paginate
