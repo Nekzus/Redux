d6cce83a2cffffedb06ba9ac89a9ecc3906c19ec
Nekzus <willianrazera@outlook.com>
2020-01-04 15:23:42 -0300

    Upload automatico via comando

--- utils/setters/signFooter.lua
+++ utils/setters/signFooter.lua
@@ -1,6 +1,7 @@
 function signFooter(embed, author, lang)
 	embed:timestamp(discordia.Date():toISO("T", "Z"))
 	embed:footer(localize("${commandRanBy}", lang, author.tag))
+	return embed
 end
 
 return signFooter
